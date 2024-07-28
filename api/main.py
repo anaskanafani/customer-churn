49892c5b1528476ae294d9a84b68e8aae65ee9cf
anaskanafani <anas.kanafani10@outlook.com>
2024-07-28 21:59:35 +0400

    add corr data to the api

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import pickle
 import pandas as pd
+import json
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
+
 
 class Customer(BaseModel):
     SeniorCitizen: int
@@ -19,7 +24,8 @@ class Customer(BaseModel):
     PaymentMethod: int
     MonthlyCharges: float
     TotalCharges: float
-    
+
+
 class RegressorCustomer(BaseModel):
     SeniorCitizen: int
     Partner: int
@@ -38,32 +44,49 @@ class RegressorCustomer(BaseModel):
     PaymentMethod: int
     MonthlyCharges: float
     Churn: int
-    
+
+
 with open('classifier.pkl', 'rb') as file:
     classifier = pickle.load(file)
-    
+
 with open('regressor.pkl', 'rb') as file:
     regressor = pickle.load(file)
+
+with open("total_charges_correlation.json", "r") as file:
+    total_charges_data = json.load(file)
     
+with open("churn_correlation.json", "r") as file:
+    churn_data = json.load(file)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/total_charges_correlation")
+async def total_charges_correlation():
+    return JSONResponse(content=total_charges_data)
+
+@app.get("/churn_correlation")
+async def churn_correlation():
+    return JSONResponse(content=churn_data)
+
 @app.post("/classify-churn")
 async def predict(customer: Customer):
     customer = pd.DataFrame([customer.model_dump()])
     prediction = classifier.predict(customer)
-    
+
     if prediction[0] == 0:
         customer = "Not churn"
     else:
         customer = "Churn"
     return {"prediction": customer}
 
+
 @app.post("/predict-total-charges")
 async def predict_total_charges(customer: RegressorCustomer):
     customer = pd.DataFrame([customer.model_dump()])
     prediction = regressor.predict(customer)
-    
+
     return {"prediction": prediction[0]}
